8e6b7ff8c666d9dc6e130a8642c94a813cfb4b69
Mari <marinarosemartin@gmail.com>
2020-10-11 12:01:29 -0500

    everything

---
+++ routes/html-routes.js
@@ -0,0 +1,37 @@
+const path = require("path");
+
+const isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function (app) {
+  app.get("/", (req, res) => {
+    // if (req.profile) {
+    //   res.redirect("../views/partials/profile");
+    // }
+    res.render(path.join(__dirname, "../views/index"));
+  });
+  app.get("/signup", (req, res) => {
+    // if (req.profile) {
+    //   res.redirect("../views/partials/profile");
+    // }
+    res.render(path.join(__dirname, "../views/partials/signup"));
+  });
+
+  app.get("/search", (req, res) => {
+    res.render(path.join(__dirname, "../views/partials/search"));
+  });
+
+  app.get("/results", (req, res) => {
+    res.render(path.join(__dirname, "../views/partials/results"));
+  });
+
+  app.get("/login", (req, res) => {
+    if (req.profile) {
+      res.redirect("/../views/partials/profile ");
+    }
+    res.sendFile(path.join(__dirname, "../views/index"));
+  });
+
+  //   app.get("/../views/partials/profile", isAuthenticated, (req, res) => {
+  //     res.sendFile(path.join(__dirname, "../views/members.html"));
+  //   });
+};
